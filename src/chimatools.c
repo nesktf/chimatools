5fd0f9793f601a4065f99e64b90aa3001b0276ce
nesktf <nesktf@proton.me>
2025-07-20 15:41:28 -0300

    Fix wrong UV offset & add funny ASCII thing

--- src/chimatools.c
+++ src/chimatools.c
@@ -686,13 +686,28 @@ copy_texels:
     memcpy(sprites[i].name.data, images[i]->name.data, images[i]->name.length);
     sprites[i].name.length = images[i]->name.length;
 
+    /*
+     * We consider the top left as the (0,0) coordinate
+     * for the atlas image, the sprite offsets and the sprite UVs.
+     *
+     * (0,0);(0,0)                                        (sz,0);(1,0)
+     * +-------------------------------------------------------------+
+     * |  (x,y);(x/sz,y/sz)                 (x+w,y);((x+w)/sz,y/sz)  |
+     * |                      +----------+                           |
+     * |                      |          |                           |
+     * |                      |          |                           |
+     * |                      +----------+                           |
+     * |  (x,y+h);(x/sz,(y+h)/sz)     (x+w,y+h);((x+w)/sz,(y+h)/sz)  |
+     * +-------------------------------------------------------------+
+     * (0,sz);(0,1)                                      (sz,sz);(1,1)
+    */
     float uv_scale_x = (flip_x*-1.f + !flip_x*1.f)*(float)w/(float)atlas_size;
-    float uv_off_x = (float)(x + w)/(float)atlas_size;
+    float uv_off_x = (float)x/(float)atlas_size;
     sprites[i].uv_x_lin = uv_scale_x;
     sprites[i].uv_x_con = flip_x*(1.f-uv_off_x) + !flip_x*uv_off_x;
 
     float uv_scale_y = (flip_y*-1.f + !flip_y*1.f)*(float)h/(float)atlas_size;
-    float uv_off_y = (float)(y + h)/(float)atlas_size;
+    float uv_off_y = (float)y/(float)atlas_size;
     sprites[i].uv_y_lin = uv_scale_y;
     sprites[i].uv_x_con = flip_y*(1.f-uv_off_y) + !flip_y*uv_off_y;
 
